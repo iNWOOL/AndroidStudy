94432072495a306a2a897d1c79684ea290472096
c <dlsdnekd@gmail.com>
2016-05-07 17:14:46 +0900

    dd

--- T02_menu/app/src/main/java/com/example/c/t02_menu/MainActivity.java
+++ T02_menu/app/src/main/java/com/example/c/t02_menu/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView tv = (TextView) findViewById(R.id.textview);
+        registerForContextMenu(tv);
     }
 
     @Override
