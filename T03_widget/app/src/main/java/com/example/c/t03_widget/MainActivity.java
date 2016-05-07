b7514f10d5a306f5568412fdcc9f52058e0e55e6
c <dlsdnekd@gmail.com>
2016-05-07 18:13:22 +0900

    ddd

--- T03_widget/app/src/main/java/com/example/c/t03_widget/MainActivity.java
+++ T03_widget/app/src/main/java/com/example/c/t03_widget/MainActivity.java
@@ -6,7 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnTest = (Button) findViewById(R.id.btnwg);
+
+        Button btn3 = (Button)findViewById(R.id.btn3);
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText etTest =  (EditText)findViewById(R.id.etwg);
+
+                if(etTest.getText().length()==0) {
+                    Toast.makeText(MainActivity.this, "데이터를 입력해", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, etTest.getText(), Toast.LENGTH_SHORT).show();
+                    etTest.setText("잘햇네");
+                }
+            }
+        });
 
         btnTest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +58,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        RadioButton rb1 = (RadioButton) findViewById(R.id.rb1);
+        RadioGroup rg = (RadioGroup) findViewById(R.id.rgwg);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton)findViewById(checkedId);
+                Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
